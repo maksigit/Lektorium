5aebcf26e6d4de220550a1684b9d1639c258ab58
maksigit <maksi123p@gmail.com>
2018-10-29 23:14:40 +0200

    redid lesson2

--- lesson2/lesson2.js
+++ lesson2/lesson2.js
@@ -1,12 +1,7 @@
-function maxMinSum(arr) {
-  let arrLength = arr.length;
+function max(arr) {
   let max;
-  let min;
-  let sum = 0;
-  let result = "";
 
-
-  for(let i = 0; i < arrLength - 1; i++) {
+  for(let i = 0; i < arr.length - 1; i++) {
     if(arr[i] > arr[i + 1]) {
       if(max > arr[i]) {
         continue
@@ -23,9 +18,13 @@ function maxMinSum(arr) {
     }
   }
 
-  result += `MAX => ${max}; \n`;
+  return max
+}
 
-  for(let i = 0; i < arrLength - 1; i++) {
+function min(arr) {
+  let min;
+  
+  for(let i = 0; i < arr.length - 1; i++) {
     if(arr[i] < arr[i + 1]) {
       if(min < arr[i]) {
         continue
@@ -41,22 +40,48 @@ function maxMinSum(arr) {
       }
     }
   }
+  
+  return min
+}
 
-  result += `MIN => ${min}; \n`;
-
-  for (let i = 0; i < arrLength; i++) {
+function sum (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i]) {
       sum += arr[i]
     }
   }
 
-  result += `SUM => ${sum}; \n\n`;
+  return sum
+}
+
+
+function maxMinSum(arr, func) {
+  let result;
+
+  if (func === max) {
+    result = max(arr);
+  } else if (func === min) {
+    result = min(arr);
+  } else {
+    result = sum(arr);
+  }
 
   return result
 }
 
-console.log(maxMinSum([3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1]));
-console.log(maxMinSum([-1,-8,-2]));
-console.log(maxMinSum([1,7,3]));
-console.log(maxMinSum([1,undefined,3,5,-3]));
-console.log(maxMinSum([1,NaN,3,5,-3]));
+console.log('MAX =>', maxMinSum([3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1], max));
+console.log('MIN =>', maxMinSum([3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1], min));
+console.log('SUM =>', maxMinSum([3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1], sum));
+console.log('MAX =>', maxMinSum([-1,-8,-2], max));
+console.log('MIN =>', maxMinSum([-1,-8,-2], min));
+console.log('SUM =>', maxMinSum([-1,-8,-2], sum));
+console.log('MAX =>', maxMinSum([1,7,3], max));
+console.log('MIN =>', maxMinSum([1,7,3], min));
+console.log('SUM =>', maxMinSum([1,7,3], sum));
+console.log('MAX =>', maxMinSum([1,undefined,3,5,-3], max));
+console.log('MIN =>', maxMinSum([1,undefined,3,5,-3], min));
+console.log('SUM =>', maxMinSum([1,undefined,3,5,-3], sum));
+console.log('MAX =>', maxMinSum([1,NaN,3,5,-3], max));
+console.log('MIN =>', maxMinSum([1,NaN,3,5,-3], min));
+console.log('SUM =>', maxMinSum([1,NaN,3,5,-3], sum));
